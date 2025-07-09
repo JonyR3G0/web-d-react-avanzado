ebfaeba068b95beffad0528ba3f2aac686befe5d
J0n4s4n <jonh.reyes.go@gmail.com>
2025-07-08 20:31:09 -0600

    Add BasicForm component and integrate it into App (formulario)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { BasicForm } from './components/BasicForm'
 
 export const App = () => {
   return (
     <div>
       <h1>Holiwiwis!</h1>
+      <BasicForm />
     </div>
   )
 }

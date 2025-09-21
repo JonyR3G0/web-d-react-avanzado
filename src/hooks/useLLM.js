e31d83a90e7edccf84b4f755df439e544285b871
J0n4s4n <jonh.reyes.go@gmail.com>
2025-09-21 03:26:01 -0600

    fix: :bug: Arreglando el hook para que funcione adecuadamente con mi modelo

--- src/hooks/useLLM.js
+++ src/hooks/useLLM.js
@@ -4,12 +4,15 @@ export const useLLM = () => {
   // Funcion asincrona para manejar la peticion con axios
   const handleQuestion = async (userPrompt) => {
     try {
-      const res = await axios.post('http://localhost:11434/api/generate', {
-        model: 'llama2',
-        promt: userPrompt,
-        stream: true
+      const res = await axios.post('http://localhost:11434/api/chat', {
+        model: 'deepseek-r1:1.5b',
+        messages: [
+          { role: 'user', content: userPrompt }
+        ],
+        stream: false
       })
-      return res.data.response
+      console.log(res.data.message.content)
+      return res.data.message.content.replace(/<think>.*?<\/think>/gs, '')
     } catch (error) {
       console.log('error', error)
     }

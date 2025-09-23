116bc196d9bc506c3b3d427c16d0259a1d55edea
J0n4s4n <jonh.reyes.go@gmail.com>
2025-09-23 17:43:30 -0600

    feat: :sparkles: Implementando get de el archivo entero de peliculas

--- express/src/app.js
+++ express/src/app.js
@@ -2,6 +2,9 @@ require('dotenv').config()
 
 const express = require('express')
 
+const { infoPeliculas } = require('./peliculas')
+
+// * Esta es la manera para acceder a el archivo .env
 require('dotenv').config()
 
 // Sacando puerto desde .env
@@ -9,9 +12,15 @@ const PORT = process.env.PORT
 // Creamos app de express
 const app = express()
 
+// ? Cuantos get podemos tener? todos los que necesitemos
+
 // Response a la raiz simple
 app.get('/', (req, res) => {
   res.send('Hola mundo')
+})
+
+app.get('/api/peliculas', (req, res) => {
+  res.send(infoPeliculas)
 })
 
 // Lanzando la app en el puerto

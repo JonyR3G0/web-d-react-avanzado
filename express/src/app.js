67911908eeeef6e74c965d92e8cf2937c4750fd5
J0n4s4n <jonh.reyes.go@gmail.com>
2025-09-22 23:17:31 -0600

    feat: :construction: Adding dependences

---
+++ express/src/app.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+// Dot env es una libreria para administrar los archivos .env que son secretos de la applicacion (apis, credenciales de todo tipo etc)
+// En este script basico se usa para inyectar el puerto que se va a usar para levantar el servidor, tambies se ve que comento el sensei un nombre, probablemente contenido en el archivo .env tambien, asi que lo voy a agregar.
+// Claramente el .env no esta en el repositorio del sensei, y sin acceso a las clases, toca hacer ingenieria inversa para crear ese archivo.
+import express from 'express';
+import { infoPeliculas } from './peliculas.js';
+// importar express
+// Importar libreria que se usara mas adelante
+
+// Creamos app de express
+
+// Definir el puerto de escucha
+
+// Definimos el get para la raiz
+
+//  Un get para JSON raw de la libreria que se va a importar mas adelante
+
+// Otro get pero en esta ocacion es con parametros dinamicos, funcion para buscar por titulo o a;o
+
+// Otros gets para pais por ejemplo
+
+// !Un app.listen (No se para que funciona, habra que investigar despues)
+// ah, creo que solamente ejecuta algo cuando se escucha una conexion a un puerto, pero habra que ver

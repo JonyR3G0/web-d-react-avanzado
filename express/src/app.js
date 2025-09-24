1aefb3374d51e0d76efdd0bdd6aedfeaa40a0b2c
J0n4s4n <jonh.reyes.go@gmail.com>
2025-09-23 18:44:02 -0600

    feat: :construction: Añadiendo funcionalidad con querrys

--- express/src/app.js
+++ express/src/app.js
@@ -34,6 +34,19 @@ app.get('/api/peliculas/accion/titulo/:titulo/:ano', (req, res) => {
   res.send(resultados)
 })
 
+// * QUERRY
+app.get('/api/peliculas/comedia/pais/:pais', (req, res) => {
+  const pais = req.params.pais
+  const resultados = infoPeliculas.comedia.filter(pelicula => pelicula.pais === pais)
+
+//   Si tenemos querry de ordenar por año, reordenamos conforme fechas
+  if (req.query.ordenar === 'ano') {
+    return res.send(resultados.sort((a, b) => a.año - b.año))
+  }
+
+  res.send(resultados)
+}
+)
 // Lanzando la app en el puerto
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`)

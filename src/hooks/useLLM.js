d3111c9e7276eea70e0b28cf20f26c91885abd99
J0n4s4n <jonh.reyes.go@gmail.com>
2025-09-23 21:48:57 -0600

    feat: :sparkles: Simplificando la función handleQuestion para manejar la petición a la API

--- src/hooks/useLLM.js
+++ src/hooks/useLLM.js
@@ -3,19 +3,8 @@ import axios from 'axios'
 export const useLLM = () => {
   // Funcion asincrona para manejar la peticion con axios
   const handleQuestion = async (userPrompt) => {
-    try {
-      const res = await axios.post('http://localhost:11434/api/chat', {
-        model: 'deepseek-r1:1.5b',
-        messages: [
-          { role: 'user', content: userPrompt }
-        ],
-        stream: false
-      })
-      console.log(res.data.message.content)
-      return res.data.message.content.replace(/<think>.*?<\/think>/gs, '')
-    } catch (error) {
-      console.log('error', error)
-    }
+    const res = await axios.post('http://localhost:3003/api/generate', { userPrompt })
+    return res
   }
   return { handleQuestion }
 }

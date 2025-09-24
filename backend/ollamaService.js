f6b06445a2d331c0de69a3967e92d3b07c43e62d
J0n4s4n <jonh.reyes.go@gmail.com>
2025-09-23 21:32:42 -0600

    feat: :construction: Editando el script del custom hook para poder se consumido por el servidor

---
+++ backend/ollamaService.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+export const generateFromPrompt = async (prompt) => {
+  // Funcion asincrona para manejar la peticion con axios
+  const res = await axios.post('http://localhost:11434/api/chat', {
+    model: 'deepseek-r1:1.5b',
+    messages: [
+      { role: 'user', content: prompt }
+    ],
+    stream: false
+  })
+  return res.data.message.content.replace(/<think>.*?<\/think>/gs, '')
+}

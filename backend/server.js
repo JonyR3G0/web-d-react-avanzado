9d581318b81c411626ba7b40d6bf597785e976d1
J0n4s4n <jonh.reyes.go@gmail.com>
2025-09-23 23:24:37 -0600

    feat: :sparkles: Implementar rutas para obtener y agregar mensajes en la API

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,33 @@ app.post('/api/generate', async (req, res) => {
   }
 })
 
+// Get para obtener mensajes desde el front
+app.get('/api/messages', async (req, res) => {
+  await db.read()
+  res.json(db.data.messages)
+})
+
+// Post: ruta para agregar mensaje nuevo
+app.post('/api/messages', async (req, res) => {
+  // text, sender
+  const { text, sender } = req.body
+  // Fallback por si faltan datos en la request
+  if (!text || !sender) {
+    // salimos si no hay uno u otro
+    return res.status(400).json({ error: 'Faltan datos' })
+  }
+  const newMessage = {
+    id: Date.now(),
+    text,
+    sender,
+    timestamp: new Date().toISOString(),
+  }
+  await db.read()
+  db.data.messages.push(newMessage)
+  await db.write()
+  res.status(201).json(newMessage)
+})
+
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto http://localhost:${PORT}`)
 })

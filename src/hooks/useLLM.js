6d86a348cc208ead2bc2b628b21402aa8291b5d5
J0n4s4n <jonh.reyes.go@gmail.com>
2025-09-23 21:49:54 -0600

    fix: :bug: Corrigiendo el nombre del parámetro en la función handleQuestion para mantener la consistencia

--- src/hooks/useLLM.js
+++ src/hooks/useLLM.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 export const useLLM = () => {
   // Funcion asincrona para manejar la peticion con axios
-  const handleQuestion = async (userPrompt) => {
-    const res = await axios.post('http://localhost:3003/api/generate', { userPrompt })
+  const handleQuestion = async (prompt) => {
+    const res = await axios.post('http://localhost:3003/api/generate', { prompt })
     return res
   }
   return { handleQuestion }
